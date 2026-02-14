0632070abc288c48d1085295a242c72f0592eeca
zimbatm <zimbatm@zimbatm.com>
2026-02-14 11:23:58 +0100

    devshell: add tmdoc wrapper

--- devshell.nix
+++ devshell.nix
@@ -3,6 +3,10 @@ pkgs.mkShell {
   packages = [
     pkgs.bun
     pkgs.biome
+    (pkgs.writeShellScriptBin "tmdoc" ''
+      root="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+      exec bun "$root/src/main.ts" "$@"
+    '')
   ];
 
   env = { };
